1c3cd54b81aff1e68d88811de17e91fec8422e7f
Steven Holms <superlinkx@gmail.com>
2016-05-03 19:20:52 -0600

    V0.1 Working on Windows

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 #Simple SMTP client written in Go
+
+Usage: quickmailer -server smtp.gmail.com -passwd password -port 587 -from myemail@gmail.com -to theiremail@example.com -attachDir "/home/user/someDir"
+
+Or: quickmailer.exe -server smtp.gmail.com -passwd password -port 587 -from myemail@gmail.com -to theiremail@example.com -attachDir "C:\Users\Me\someDir"
+
+Additonal parameters:
+
+-body
+-subject
+-attachFile
--- quickmailer.go
+++ quickmailer.go
@@ -3,54 +3,67 @@ package main
 import (
 	"flag"
 	"net/smtp"
+	"path/filepath"
 
 	"github.com/jordan-wright/email"
 )
 
-func main() {
-	var (
-		server     string
-		port       string
-		passwd     string
-		from       string
-		to         string
-		subject    string
-		body       string
-		attachFile string
-		attachDir  string
-	)
+type emailParams struct {
+	server     string
+	sendserver string
+	port       string
+	passwd     string
+	from       string
+	to         string
+	subject    string
+	body       string
+	attachFile string
+	attachDir  string
+}
 
-	flag.StringVar(&server, "server", "", "smtp server without port to use")
-	flag.StringVar(&port, "port", "587", "smtp port to use")
-	flag.StringVar(&passwd, "passwd", "", "password for smtp auth")
-	flag.StringVar(&from, "from", "", "email to send from")
-	flag.StringVar(&to, "to", "", "email to send to")
-	flag.StringVar(&subject, "subject", "Your Files", "subject line")
-	flag.StringVar(&body, "body", "See attached", "some body text")
-	flag.StringVar(&attachFile, "attach", "", "single file to attach")
-	flag.StringVar(&attachDir, "attachDir", "", "directory to attach files from")
+func main() {
+	settings := getArgs()
+
+	e := email.NewEmail()
+	e.From = "<" + settings.from + ">"
+	e.To = []string{settings.to}
+	e.Subject = settings.subject
+	e.Text = []byte(settings.body)
+
+	if settings.attachFile != "" {
+		e.AttachFile(settings.attachFile)
+	}
+
+	if settings.attachDir != "" {
+		files, _ := filepath.Glob(settings.attachDir + "/*")
+		for _, elem := range files {
+			e.AttachFile(elem)
+		}
+	}
+
+	e.Send(settings.sendserver, smtp.PlainAuth("", settings.from, settings.passwd, settings.server))
+}
+
+func getArgs() emailParams {
+	var params emailParams
+
+	flag.StringVar(&params.server, "server", "", "smtp server without port to use")
+	flag.StringVar(&params.port, "port", "587", "smtp port to use")
+	flag.StringVar(&params.passwd, "passwd", "", "password for smtp auth")
+	flag.StringVar(&params.from, "from", "", "email to send from")
+	flag.StringVar(&params.to, "to", "", "email to send to")
+	flag.StringVar(&params.subject, "subject", "Your Files", "subject line")
+	flag.StringVar(&params.body, "body", "See attached", "some body text")
+	flag.StringVar(&params.attachFile, "attach", "", "single file to attach")
+	flag.StringVar(&params.attachDir, "attachDir", "", "directory to attach files from")
 
 	flag.Parse()
 
-	if (server == "") || (port == "") || (passwd == "") || (from == "") || (to == "") {
+	if (params.server == "") || (params.port == "") || (params.passwd == "") || (params.from == "") || (params.to == "") {
 		panic("Required flags: -server, -passwd, -port, -from, -to")
 	}
 
-	sendserver := server + ":" + port
+	params.sendserver = params.server + ":" + params.port
 
-	e := email.NewEmail()
-	e.From = "<" + from + ">"
-	e.To = []string{to}
-	e.Subject = subject
-	e.Text = []byte(body)
-
-	if attachFile != "" {
-		e.AttachFile(attachFile)
-	}
-
-	if attachDir != "" {
-
-	}
-
-	e.Send(sendserver, smtp.PlainAuth("", from, passwd, server))
+	return params
 }
